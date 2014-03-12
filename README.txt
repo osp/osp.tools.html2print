c4665c118eeb09f1f9b34dc134ee6247a00ef8aa
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-03-12 18:14:24 +0100

    Crop marks seem ok.

--- README.txt
+++ README.txt
@@ -2,3 +2,15 @@ HTML 2 print
 ============
 
 This little tool is a boilerplate, a minimal example to start a print project using HTML, less/CSS and Javascript/Jquery to design it.
+
+You can't use less.js on a local file (URL starting with "file:///"). 
+To bypass this limitation, you can run a simple webserver with python. 
+To do so:
+
+- Open a terminal.
+- Run those sommands:
+
+    cd /path/to/this/directory/
+    python -m SimpleHTTPServer
+
+- Visit the URL "http://localhost:8000/"!
--- print.js
+++ print.js
@@ -1,14 +1,20 @@
-// __________________________________ PRINT MARKS __________________________________ //
-doc_height = $("body").height();
-page_height = $("#master-page").height(); 
-//page_height = 22; 
-nb_page = Math.ceil(doc_height/page_height) + 4;
-//gutter = parseInt($("#fakepage").css("top"));  // = 1cm
+$(window).load(function(){
+    // __________________________________ PRINT MARKS __________________________________ //
+    doc_height = $("body").height();
+    page_height = $("#master-page").height(); 
+    //page_height = 22; 
+    nb_page = Math.ceil(doc_height/page_height);
+    //gutter = parseInt($("#fakepage").css("top"));  // = 1cm
 
+    console.log(doc_height);
+    console.log(page_height);
+    console.log(nb_page);
 
-for (i = 0; i <= nb_page; i++){
-    $("#master-page").clone().css({'outline-offset': '-1cm', 'display': 'block'}).attr("id","page"+i).insertBefore($("#master-page"));
-        
-    // FOLIO
-    //$("#fakepage-p" + i + " .folio").html("<p>page " + (i+1) + "</p>");
-}
+    for (i = 0; i < nb_page; i++){
+        $("#master-page").clone().addClass("preview-page").attr("id","page"+i).insertBefore($("#master-page"));
+            
+        // FOLIO
+        //$("#fakepage-p" + i + " .folio").html("<p>page " + (i+1) + "</p>");
+    }
+    $("#master-page").hide();
+});
