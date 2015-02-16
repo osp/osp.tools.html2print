6b1c9f13902d7181858069409f9de041c04d677f
Alexandre Leray <alexandre@stdin.fr>
2015-02-16 01:02:58 +0100

    Séparation du javascript

--- html2print.js
+++ html2print.js
@@ -1,11 +1,4 @@
-// Choose the number of pages of the document
-var nb_page = 7;
-
-
-// Loads main content into <section id="story">
-$("#my-story").load("content.html");
-
-$(window).load(function(){
+$(function() {
     // ________________________________ PREVIEW __________________________________ //
     $("#preview").click(function(e){
         e.preventDefault();
@@ -56,13 +49,6 @@ $(window).load(function(){
     });
 
 
-    // __________________________________ PRINT MARKS __________________________________ //
-    for (i = 1; i < nb_page; i++){
-        $("#master-page").clone().attr("id","page-"+i).insertBefore($("#master-page"));
-    }
-    $("#master-page").hide();
-
-
     // __________________________________ TOC __________________________________ //
     $(".paper").each(function(){
         page = $(this).attr("id");
@@ -73,26 +59,5 @@ $(window).load(function(){
         e.preventDefault();
         $(this).toggleClass("button-active");
         $("#toc-pages").toggle();
-    });
-
-
-    // __________________________________ MOVEABLE ELEMENTS __________________________________ //
-    $("div.moveable").
-        append("<div class='properties button'>Properties</div>").
-        draggable({
-                cursor: "move",
-                stack: "div.moveable", 
-        }).
-        resizable();
-
-    $('.properties').on('click', function() {
-        var top = $(this).parent().css('top');
-        var left = $(this).parent().css('left');
-        var width = $(this).parent().width();
-        var height = $(this).parent().height();
-        var p = new Popelt({
-            title: "Properties to copy/paste into this object's style attribute.",
-            content: 'style="top: ' + top + '; left: ' + left + '; width: ' + width + 'px; height: ' + height + 'px;"',
-        }).show();
     });
 });
