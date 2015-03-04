e96faa393a82f92cc9f1c7b24e3a1d17b79788e1
colm <mail@colm.be>
2015-03-04 17:25:59 +0100

    mega clean

--- colorSeparation/rgb2cmyk.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-#
-# USAGE:
-# ./rgb2cmyk.sh input.pdf
-#
-# OUTPUT:
-# input-cmyk.pdf
-
-gs -dNOPAUSE -sDEVICE=pdfwrite -sColorConversionStrategy=CMYK -dProcessColorModel=/DeviceCMYK -dNOPAUSE -dBATCH  -sOUTPUTFILE=$(basename ${1} .pdf)-cmyk.pdf ${1}
--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 $(function() {
     // Choose the number of pages of the document
-    var nb_page = 7;
+    var nb_page = 4;
 
     for (i = 1; i < nb_page; i++){
         $("#master-page").clone().attr("id","page-"+i).insertBefore($("#master-page"));
@@ -15,7 +15,7 @@ $(function() {
         append("<div class='properties button'>Properties</div>").
         draggable({
                 cursor: "move",
-                stack: "div.moveable", 
+                stack: "div.moveable",
         }).
         resizable();
 
